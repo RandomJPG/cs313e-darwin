b48aa2274fbdf5cefef53003a5664f1ca2dca47f
Jacob Garcia <jpgarcia39@yahoo.com>
2014-11-04 18:30:33 -0600

    Added name and program list to Species, added width and height to Darwin class

--- Darwin.py
+++ Darwin.py
@@ -4,10 +4,24 @@
 # -------------------------------
 
 class Darwin:
-
-    class Species:
+    
+    def __init__(self, height, width):
+        assert type(height) is int
+        assert type(width) is int
         
-        def __init__(self, name):
-            self.name = name
-            
-    class Creature:
+        self.h = height
+        self.w = width
+
+class Species:
+    
+    def __init__(self, name):
+        assert type(name) is str
+        
+        self.name = name[0]
+        self.program = []
+                
+    def addInstruction(self, instruction):
+        assert type(instruction) is str
+        
+        self.program.append(instruction)
+        
--- TestDarwin.py
+++ TestDarwin.py
@@ -17,7 +17,59 @@ import Darwin
 
 class TestDarwin (TestCase) :
 
+    # -------
+    # Species
+    # -------
+    
+    def test_species_1(self):
+        # Short name
+        a = Darwin.Species("a")
+        self.assertEqual(a.name, "a") 
 
+    def test_species_2(self):
+        # Long name
+        a = Darwin.Species("Bob")
+        self.assertEqual(a.name, "B")
+        
+    def test_species_3(self):
+        # No commands
+        a = Darwin.Species("a")
+        self.assertEqual(a.program, [])
+        
+    def test_species_4(self):
+        # Length of program list
+        a = Darwin.Species("a")
+        a.addInstruction("1")
+        self.assertEqual(len(a.program), 1)
+    
+    # ------
+    # Darwin
+    # ------
+    
+    def test_darwin_1(self):
+        # Same height/width, small grid
+        game = Darwin.Darwin(8,8)
+        self.assertEqual(game.h, 8)
+        self.assertEqual(game.w, 8)
+        
+    def test_darwin_2(self):
+        # Different height/width, small grid
+        game = Darwin.Darwin(7, 9)
+        self.assertEqual(game.h, 7)
+        self.assertEqual(game.w, 9)
+        
+    def test_darwin_3(self):
+        # Same height/width, big grid
+        game = Darwin.Darwin(72, 72)
+        self.assertEqual(game.h, 72)
+        self.assertEqual(game.w, 72)
+        
+    def test_darwin_4(self):
+        # Different height/width, big grid
+        game = Darwin.Darwin(90, 100)
+        self.assertEqual(game.h, 90)
+        self.assertEqual(game.w, 100)
+          
 # ----
 # main
 # ----
